13d122435808a6a855663caaab69b5733e08c916
albiorixUA <steblynamaxim@gmail.com>
2021-12-03 18:04:13 +0200

    add homework#6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const refs = {
+  input: document.querySelector("#validation-input"),
+};
+refs.input.addEventListener("blur", CheckInputLength);
+
+function CheckInputLength(event) {
+  if (event.currentTarget.value.length === Number(refs.input.dataset.length)) {
+    refs.input.classList.add("valid");
+    refs.input.classList.remove("invalid");
+  } else {
+    refs.input.classList.add("invalid");
+    refs.input.classList.remove("valid");
+  }
+}
---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const refs = {
+  input: document.querySelector("#font-size-control"),
+  span: document.querySelector("#text"),
+};
+refs.input.addEventListener("input", onInputChange);
+function onInputChange(event) {
+  refs.span.style.fontSize = `${event.currentTarget.value}px`;
+}
